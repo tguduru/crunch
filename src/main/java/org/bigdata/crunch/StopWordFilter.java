4e2cefe8fa87770658ac38731007cad9b0b685d3
ghstreddy <guduru.thirupathireddy@gmail.com>
2013-11-21 21:01:15 -0800

    added crunch default example

---
+++ src/main/java/org/bigdata/crunch/StopWordFilter.java
@@ -0,0 +1,33 @@
+package org.bigdata.crunch;
+
+import java.util.Set;
+
+import org.apache.crunch.FilterFn;
+
+import com.google.common.collect.ImmutableSet;
+
+
+/**
+ * A filter that removes known stop words.
+ */
+public class StopWordFilter extends FilterFn<String> {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -3221419251509279149L;
+	// English stop words, borrowed from Lucene.
+    private static final Set<String> STOP_WORDS = ImmutableSet.copyOf(new String[] {
+        "a", "and", "are", "as", "at", "be", "but", "by",
+        "for", "if", "in", "into", "is", "it",
+        "no", "not", "of", "on", "or", "s", "such",
+        "t", "that", "the", "their", "then", "there", "these",
+        "they", "this", "to", "was", "will", "with"
+    });
+
+    @Override
+    public boolean accept(String word) {
+        return !STOP_WORDS.contains(word);
+    }
+
+}
---
+++ src/main/java/org/bigdata/crunch/Tokenizer.java
@@ -0,0 +1,26 @@
+package org.bigdata.crunch;
+
+
+import org.apache.crunch.DoFn;
+import org.apache.crunch.Emitter;
+
+import com.google.common.base.Splitter;
+
+
+/**
+ * Splits a line of text, filtering known stop words.
+ */
+public class Tokenizer extends DoFn<String, String> {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 5277959463863910994L;
+	private static final Splitter SPLITTER = Splitter.onPattern("\\s+").omitEmptyStrings();
+
+    @Override
+    public void process(String line, Emitter<String> emitter) {
+        for (String word : SPLITTER.split(line)) {
+            emitter.emit(word);
+        }
+    }
+}
